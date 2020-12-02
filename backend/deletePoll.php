73498ee9406c160a014563cfe2704eae1fe9dc2a
Tuomas Puro <tuomas.puro@esedu.fi>
2020-12-02 14:28:20 +0200

    Vähän hienosäätöä poistotoimintoon

--- backend/deletePoll.php
+++ backend/deletePoll.php
@@ -1,45 +1,44 @@
 <?php
-// Tarkista onko kirjauduttu
 // TODO
+// * Check if user is logged in
 
+
+// Check if $_GET['id'] is present
 if (!isset($_GET['id'])){
-    header('Location: ../index.php');
+    $type = 'error';
+    $msg = 'Tried to do something stupid!';
+    header("Location: ../index.php?type={$type}&msg={$msg}");
 }
-
 $poll_id = $_GET['id'];
 
 include_once 'pdo-connect.php';
 
+$data = array();
+
 try {
+    // Deleting options
     $stmt = $conn->prepare("DELETE FROM option WHERE poll_id = :pollid;");
     $stmt->bindParam(':pollid', $poll_id);
 
     if ($stmt->execute() == false){
-        $data = array(
-            'error' => 'Error occured!'
-        );
+        $data['error']['options']=  'Error occured deleting options!';
     } else {
-        $data = array(
-            'success' => 'Vote successfull!'
-        );
+        $data['success']['options'] = 'Deleting options successfull!';
     }
 
+    // Deleting polls
     $stmt = $conn->prepare("DELETE FROM poll WHERE id = :pollid;");
     $stmt->bindParam(':pollid', $poll_id);
 
     if ($stmt->execute() == false){
-        $data = array(
-            'error' => 'Error occured!'
-        );
+        $data['error']['poll'] = 'Error occured deleting poll!';
     } else {
-        $data = array(
-            'success' => 'Vote successfull!'
-        );
+        $data['success']['poll'] = 'Deleting pol successfull!';
     }
 
 } catch (PDOException $e) {
     $data = array(
-        'error' => 'Tapahtui joku virhe!!'
+        'error' => 'Error occured in SQL statement'
     );
 }
 
